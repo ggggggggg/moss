698f22efdef32b6700a6cdc03e6db131d4cd2dce
ggggggggg <ggggggggg@users.noreply.github.com>
2024-06-28 17:03:21 -0600

    dc working!

--- massp/__init__.py
+++ massp/__init__.py
@@ -2,3 +2,4 @@ from .ljhfiles import LJHFile
 from . import pulse_algorithms
 from .noise_algorithms import noise_psd, autocorrelation
 from .filters import fourier_filter
+from .drift_correction import drift_correct
---
+++ massp/time_algorithms.py
@@ -0,0 +1,21 @@
+import numpy as np
+import numba
+
+@numba.njit
+def python_nearest_arrivals(reference_times, other_times):
+    """Identical to nearest_arrivals(...)."""
+    nearest_after_index = np.searchsorted(other_times, reference_times)
+    last_index = np.searchsorted(nearest_after_index, other_times.size, side="left")
+    first_index = np.searchsorted(nearest_after_index, 1)
+
+    nearest_before_index = np.copy(nearest_after_index)
+    nearest_before_index[:first_index] = 1
+    nearest_before_index -= 1
+    before_times = reference_times - other_times[nearest_before_index]
+    before_times[:first_index] = np.Inf
+
+    nearest_after_index[last_index:] = other_times.size - 1
+    after_times = other_times[nearest_after_index] - reference_times
+    after_times[last_index:] = np.Inf
+
+    return before_times, after_times
