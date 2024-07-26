944e56a57d1cf67f42b731cfaba766b54fb388b7
ggggggggg <ggggggggg@users.noreply.github.com>
2024-07-26 15:11:30 -0600

    changes

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -2,11 +2,14 @@ import pytest
 import subprocess
 
 
-# def test_example_marimo():
-#     import moss
-#     import moss.example_marimo
-#     moss.example_marimo.app.run()
 
+def test_ljh_mnkalpha():
+    #enforce the order by doing it in the same function
+    # parquet_after_ljh_mnkalpha needs to open files written by ljh_mnkalpha
+    from . import ljh_mnkalpha as notebook
+    notebook.app.run()
+    # from . import parquet_after_ljh_mnkalpha as notebook2
+    # notebook2.app.run()
 
 def test_off_ebit():
     from . import off_ebit as notebook
