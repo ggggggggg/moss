c397eb68a399058189b588809a44a2fd4bf313ee
jsh162 <joshua.ho@nist.gov>
2024-08-01 11:11:28 -0600

    Created ini file and ran monkeytype on multifit,

--- moss/__main__.py
+++ moss/__main__.py
@@ -1,5 +1,5 @@
 # mypackage/__main__.py # type: ignore
-from . import misc # type: ignore
-
+ # type: ignore
 if __name__ == "__main__": # type: ignore
+    from . import misc
     misc.launch_examples() # type: ignore
